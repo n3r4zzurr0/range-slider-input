ffe9be3724362af29d78e5214bf8b6ab69a74fdf
Utkarsh Verma <utkarsh.verma@hotmail.com>
2022-11-21 11:35:22 +0530

    Build instructions

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    'rangeslider': './src/index.style.js',
+    'rangeslider.nostyle': './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].umd.min.js',
+    library: 'rangeSlider',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  }
+}
